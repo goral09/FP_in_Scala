23a8726b8ae77230262752233c750bedac0afcef
Mateusz Gorski <goral09@gmail.com>
2014-09-08 10:41:49 +0200

    few new solutions and tests

--- src/main/scala/Chapter3.scala
+++ src/main/scala/Chapter3.scala
@@ -1,11 +1,21 @@
 package com.goral09.fp_in_scala.chapter3
 
 
-sealed trait List[+A]
-case class Cons[+A](h: A, t: List[A]) extends List[A]
-case object Nil extends List[Nothing]
+sealed trait List[+A] { 
+  def isEmpty: Boolean
+}
+case class Cons[+A](h: A, t: List[A]) extends List[A] {
+  def isEmpty = false
+}
+case object Nil extends List[Nothing] {
+  def isEmpty = true
+}
 
 object List {
+  def apply[A](els: A*): List[A] = {
+    if(els.isEmpty) Nil
+    else Cons(els.head, apply(els.tail: _*))
+  }
    def sum(l: List[Int]): Int= l match {
     case Nil => 0
     case Cons(h, tail) => h + sum(tail)
@@ -15,6 +25,11 @@ object List {
     case Nil => 1.0
     case Cons(0.0, _) => 0.0
     case Cons(h, tail) => h * product(tail)
+  }
+
+  def head[A](l: List[A]): A = l match {
+    case Nil => throw new NoSuchElementException("No head in empty list")
+    case Cons(h,t) => h
   }
 
   def tail[A](l: List[A]): List[A] = l match {
@@ -63,5 +78,36 @@ object List {
   }
 
   def sum2(l: List[Int]) = foldRight(0)(l)(_+_)
-  def product2(l: List[Dobule]) = foldRight(1.0)(l)(_*_)
+
+  def product2(l: List[Double]) = foldRight(1.0)(l)(_*_)
+
+  def length[A](l: List[A]): Int = l match {
+    case Nil => 0
+    case Cons(h, t) => 1 + length(t)
+  }
+
+  @annotation.tailrec
+  def foldLeft[A,B](start: B)(l: List[A])(f: (B,A) => B): B = l match {
+    case Nil => start
+    case Cons(h, t) => foldLeft(f(start, h))(t)(f)
+  }
+
+  def sum3(l: List[Int]): Int = foldLeft(0)(l)(_ + _)
+  def product3(l: List[Double]) = foldLeft(1.0)(l)(_ * _)
+  def length2[A](l: List[A]): Int = foldLeft(0)(l)((acc,h) => acc + 1)
+
+  def reverse[A](l: List[A]): List[A] =  {
+    var buff: List[A] = Nil
+    var these = l
+    while(!l.isEmpty) {
+      buff = Cons(List.head(l), buff)
+      these = List.tail(these)
+    }
+    buff
+  }
+  def reverse2[A](l: List[A]): List[A] = List.foldLeft(List[A]())(l)((acc,curr) => Cons(curr, acc))
+
+  def foldLeftViaFoldRight[A, B](start: B)(l: List[A])(f: (B,A) => B): B = ???
+  def foldRightViaFoldLeft[A, B](start: B)(l: List[A])(f: (A,B) => B): B = ???
+
 }
--- src/test/scala/Chapter3_ListSpec.scala
+++ src/test/scala/Chapter3_ListSpec.scala
@@ -55,5 +55,58 @@ class ListSpec extends Specification {
 
       List.init(l) === Cons(1, Cons(2, Cons(3, Nil)))
     }
+
+    "properly count length of list" in {
+      val l = Cons(1, Cons(2, Cons(3, Cons(4, Nil))))
+
+      List.length(l) === 4
+    }
+
+    "properly sum of elements of the list using foldRight" in {
+      val l = Cons(1, Cons(2, Cons(3, Cons(4, Nil))))
+
+      List.sum2(l) === 10
+    }
+
+    "properly count product of elements of the list using foldRight" in {
+      val l = Cons(1.0, Cons(2.0, Cons(3.0, Cons(4.0, Nil))))
+
+      List.product2(l) === 24
+    }
+
+    "foldLeft should properly count sum of elements" in {
+      val l = Cons(1, Cons(2, Cons(3, Cons(4, Nil))))
+
+
+      List.sum3(l) === 10
+    }
+
+    "foldLeft should properly count product of elements" in {
+      val l = Cons(1.0, Cons(2.0, Cons(3.0, Cons(4.0, Nil))))
+
+      List.product3(l) === 24
+    }
+
+    "foldLeft should properly compute length of the list" in {
+      val l = Cons(1, Cons(2, Cons(3, Cons(4, Nil))))
+
+      List.length2(l) === 4
+    }
+
+    "list should be properly reverted" in {
+      "using foldLeft" in {
+        val l = Cons(1, Cons(2, Cons(3, Cons(4, Nil))))
+
+        List.reverse2(l) === Cons(4, Cons(3, Cons(2, Cons(1, Nil))))
+      }
+
+      "without using fold" in {
+        val l = Cons(1, Cons(2, Cons(3, Cons(4, Nil))))
+
+        pending 
+
+        List.reverse(l) === Cons(4, Cons(3, Cons(2, Cons(1, Nil))))
+      }
+    }
   }
 }
